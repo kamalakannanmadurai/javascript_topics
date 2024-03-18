c31c34ee2d30f0c2cf0c63ece4fff6a1b7bf32d5
Shashi Kanth Kontham <shashi.kontham@atmecs.com>
2024-03-18 16:00:15 +0530

    changes for for loop

--- ind.js
+++ ind.js
@@ -1,16 +1,22 @@
-for(let i=0;i<10;i++){
-console.log(i);
-}
-const check = {a:1,b:2};
-
+// for(let i=0;i<10;i++){
+// console.log(i);
+// }
+const check = {adam:32,brian:40,charlie:58,dave:89,envy:12};
+let minval = 10;
+let maxval = 35;
 for (let key in check){
-console.log(key,check[key]);
-}
-const check2 = [1,2,3,4,5];
-for (let key in check2){
-    console.log(key,check2[key]);
-    }
-
-for (let key of check2){
+    console.log(key);
+    if(maxval>=check[key]>=minval ){
         console.log(key);
-        }
+    }
+}
+// const check2 = [10,9,8,7,6];
+// for (let key in check2){
+//     console.log(key,check2[key]);
+//     }
+//     for (let key in check){
+//         console.log(key,check[key]);
+//         }
+// for (let key of check){
+//         console.log(key);
+//         }
