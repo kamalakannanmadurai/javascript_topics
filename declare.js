7f54b3063692a5c6b36f0bf026abb6181c66d343
Shashi Kanth Kontham <shashi.kontham@atmecs.com>
2024-03-20 10:45:23 +0530

    changes declare code

---
+++ declare.js
@@ -0,0 +1,9 @@
+// Initialize the variable x to a value of 1
+var x = 1;
+// Initialize the variable y to a value of 2
+let y = 2;
+// Initialize the variable z to a value of 3
+// Note that variables with the const keywords
+// have to be initialized as soon as they are
+// declared otherwise, you will get an error.
+const z = 3;
